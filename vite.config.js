081f501afec938f8d37f453e9e11ce6931538b57
jadi <jawadmunir158@gmail.com>
2024-11-12 20:49:57 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Pet-Pall/', // Ensure this points to your repository name
+  base: '/Pets/', // Ensure this points to your repository name
   plugins: [react()],
 })
